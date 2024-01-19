e9b71884fce30b08fb026abf9d2b2fa0fc199997
DGO2101 <pit_olimpo@yahoo.com>
2024-01-19 11:33:59 +0100

    implementar la carpeta "repositorio" para no trabajar directamente con el controller

---
+++ Repositorio/IRepositorio/IRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace MagicVill.Repositorio.IRepositorio
+{
+    public interface IRepositorio<T> where T : class // se hace que la interfaz sea generico, se puede recibir cualquier tipo de entidad
+    {
+        Task crear (T entidad);
+        Task<List<T>> ObtenerTodos (Expression<Func<T, bool>>? filtro = null);
+        Task<T> Obtener(Expression<Func<T, bool>>? filtro = null, bool tracked = true);
+        Task<T> Remove(T entidad);
+        Task<T> Gravar (T entidad);
+
+        //pedirle a chatgpt que me explique bien que hace esta clase
+    }
+}
---
+++ Repositorio/Repositorio.cs
@@ -0,0 +1,37 @@
+﻿using MagicVill.Datos;
+using MagicVill.Repositorio.IRepositorio;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace MagicVill.Repositorio
+{
+    public class Repositorio<T> : IRepositorio<T> where T : class
+    {
+        private readonly AplicationDBContext _dbContext;
+        internal DbSet<T> dbset;
+        public Task crear(T entidad)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Gravar(T entidad)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Obtener(Expression<Func<T, bool>>? filtro = null, bool tracked = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> ObtenerTodos(Expression<Func<T, bool>>? filtro = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Remove(T entidad)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
